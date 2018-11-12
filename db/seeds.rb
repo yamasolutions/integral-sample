311dcbdd093fa944547368d2d6479c1044b9752d
Patrick Lindsay <ptricklindsay@gmail.com>
2018-11-12 15:40:00 -0800

    Override call to action button & add default user name and password

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
 Integral::Engine.load_seed
+
+# Override the default call to action button
+cta_item = Integral::List.first.list_items.last
+cta_item.update_attributes(title: 'Users Area', url: "/#{Integral.backend_namespace}")
