4dabfe4a8da79b0974d77e88d93e5bac7cd0555c
Patrick Lindsay <ptricklindsay@gmail.com>
2018-11-12 15:42:10 -0800

    Enable heroku deploy button

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  Rails.application.routes.default_url_options[:host] = 'https://example.org'
+  Rails.application.routes.default_url_options[:host] = "https://#{ENV['HEROKU_APP_NAME']}.herokuapp.com"
 end
